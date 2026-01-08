519c99606abd8d093793b704af3d25a254fe0f09
João Filipe <joaofilipevgomes@gmail.com>
2026-01-08 00:59:03 -0300

    Adicionando funções de criar e liberar lista

--- Lista_encadeada.c
+++ Lista_encadeada.c
@@ -7,3 +7,31 @@ typedef struct t_no{
     No* proximo;
     No* anterior;
 }No;
+
+struct t_lista
+{
+    No* primeiro;
+    No* ultimo;
+    int tamanho;
+    int capacidade;
+};
+
+Lista* criarLista(int capacidade){
+    Lista* lista = (Lista*)malloc(sizeof(Lista));
+    lista->capacidade = capacidade;
+    lista->primeiro = NULL;
+    lista->ultimo = NULL;
+    lista->tamanho = 0;
+    return lista;
+}
+
+void liberarLista(Lista* ptr_l){
+    No* atual = ptr_l->primeiro;
+    while (atual!=NULL)
+    {
+        No* temp = atual;
+        atual = ptr_l->primeiro->proximo;
+        free(temp);
+    }
+    free(ptr_l);
+}
