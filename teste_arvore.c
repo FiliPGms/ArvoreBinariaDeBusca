d9b9b2eee7606b50953eb12260eb063f66d85328
Pablo Ricardo <pabloricardo055@gmail.com>
2026-01-22 15:00:58 -0300

    Projeto finalizado

--- teste_arvore.c
+++ teste_arvore.c
@@ -2,14 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "arvore.h"
+#include "Pedido.h"
 
 int main() {
+
+    Pedido* p1 = criarPedido(
+        1,
+        "Sem cebola",
+        "João Silva",
+        "Hambúrguer",
+        25.00
+    );
+
+    Pedido* p2 = criarPedido(
+        2,
+        "Pouco sal",
+        "Maria Santos",
+        "Lasanha",
+        32.50
+    );
+
+    Pedido* p3 = criarPedido(
+        3,
+        "Bem passado",
+        "Carlos Lima",
+        "Bife Acebolado",
+        28.00
+    );
+
     ABB* minha_arvore = criar_arvore(10, NULL);
-    inserir(minha_arvore, 15, NULL);
-    inserir(minha_arvore, 3, NULL);
-    inserir(minha_arvore, 7, NULL);
-    inserir(minha_arvore, 12, NULL);
-    inserir(minha_arvore, 18, NULL);
+    inserir(minha_arvore, obterIdPedido(p1), p1);
+    inserir(minha_arvore, obterIdPedido(p2), p2);
+    inserir(minha_arvore, obterIdPedido(p3), p3);
+
+    printf("Impressão em pré-ordem: ");
+    imprimir_pre_ordem(minha_arvore);
+    printf("\n");
     printf("Impressao em ordem: ");
     imprimir_em_ordem(minha_arvore);
     printf("\n");
